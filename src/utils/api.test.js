62f3ae605edef18e2268b639291a4ae84c68b9a4
Ellis Lee <ellismckenzielee@gmail.com>
2022-08-01 16:42:08 +0100

    add test to ensure axios called with correct url

--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { getTodos } from './api.utils';
 jest.mock('axios');
-axios.get.mockReturnValue({ body: [] });
+axios.get.mockImplementation(
+	jest.fn(() => {
+		return { body: [] };
+	})
+);
 describe('testing api functions', () => {
 	describe('getTodos', () => {
 		test('should return an array of objects', async () => {
@@ -11,6 +15,14 @@ describe('testing api functions', () => {
 			const result = await getTodos();
 			// ASSERT
 			expect(Array.isArray(result)).toBe(isArray);
+		});
+		test('should be called with the correct url', async () => {
+			// ARRANGE
+			const url = 'fakeurl.com';
+			// ACT
+			const result = await getTodos(url);
+			// ASSERT
+			expect(axios.get.mock.lastCall[0]).toBe(url);
 		});
 	});
 });
