4ef268b83539c799391b510c1a98e1833cf6344b
Ellis Lee <ellismckenzielee@gmail.com>
2022-08-01 11:50:44 +0100

    add submit todo functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,19 @@
 <script>
+	let todo = '';
 	let todos = [
-		{ id: '1', username: 'ellis', content: 'go on a run' },
-		{ id: '2', username: 'ellis', content: 'clean kitchen' },
-		{ id: '3', username: 'ellis', content: 'call gran' },
-		{ id: '4', username: 'ellis', content: 'take bins out' }
+		{ id: '1', complete: false, content: 'go on a run' },
+		{ id: '2', complete: false, content: 'clean kitchen' },
+		{ id: '3', complete: false, content: 'call gran' },
+		{ id: '4', complete: false, content: 'take bins out' }
 	];
+	function handleSubmit(event) {
+		console.log('submit');
+		todos.push({ complete: false, content: todo, id: Date.now().toLocaleString() });
+		todos = todos;
+	}
+	$: {
+		console.log(todo);
+	}
 </script>
 
 <h1>Todos</h1>
@@ -13,3 +22,7 @@
 		<li>{todo.content}</li>
 	{/each}
 </ul>
+<form on:submit|preventDefault={handleSubmit}>
+	<input bind:value={todo} type="text" />
+	<button>Add</button>
+</form>
