084e5f566a62e9aa33084bab48111a06e5f6a3a3
Ellis Lee <ellismckenzielee@gmail.com>
2022-08-01 09:42:10 +0100

    create basic svelte boilerplate

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	let todos = [
+		{ id: '1', username: 'ellis', content: 'go on a run' },
+		{ id: '2', username: 'ellis', content: 'clean kitchen' },
+		{ id: '3', username: 'ellis', content: 'call gran' },
+		{ id: '4', username: 'ellis', content: 'take bins out' }
+	];
+</script>
+
+<h1>Todos</h1>
+<ul>
+	{#each todos as todo}
+		<li>{todo.content}</li>
+	{/each}
+</ul>
